13854d5431db7a3ef42fe45d63f8b7b203c4eff9
stevenalp207 <s.alpizar207@gmail.com>
2026-02-22 17:17:21 -0600

    feat: update service worker revision and modify catequesis group presenter names for clarity

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qg0pj5votpo"
+    "revision": "0.4onh2o13oo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
