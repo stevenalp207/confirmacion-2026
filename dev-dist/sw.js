a46219db90390b6999f0a649c357eb9bbc81b0c8
stevenalp207 <s.alpizar207@gmail.com>
2026-03-13 18:39:17 -0600

    fix: update revision for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cjvj1potp38"
+    "revision": "0.gnfcli4oh3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
